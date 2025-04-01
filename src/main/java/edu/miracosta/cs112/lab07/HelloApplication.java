d4b11790c77d6d084248a5385c49c5203992a761
MCCLAB560WD05 <jzoch7@gmail.com>
2025-04-01 12:48:09 -0700

    done with part 2

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -1,6 +1,8 @@
 package edu.miracosta.cs112.lab07;//package name here depending on your IDE
 
 import javafx.application.Application;  //abstract class used for JavaFX GUI's
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;              //class for GUI window
 import javafx.scene.Scene;              //class for specific view in GUI window
 import javafx.scene.layout.VBox;        //class for layout pane, organized top-to-bottom
@@ -19,8 +21,25 @@ public class HelloApplication extends Application  { //inheriting core functiona
     }
 
     // TODO: follow steps 2-9 in README.md to create a start method
+    public void start(Stage primaryStage){
+        primaryStage.setTitle("Hello,GUI: Jake Z");
+        StackPane layout = new StackPane();
+        Scene scene = new Scene(layout ,300,300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
+        Label label = new Label();
+        label.setText("Hello GUI World");
+        layout.getChildren().add(label);
+        AnchorPane anchorPane = new AnchorPane();
+        anchorPane.setBottomAnchor(label,0.0);
+        anchorPane.setRightAnchor(label, 0.0);
+        anchorPane.getChildren().add(label);
+        layout.getChildren().add(anchorPane);
 
-    // TODO: follow steps 10-21 in README.md to add objects to your layout (inside start)
+
+    }
+
 
     // TODO: follow steps 22-34 in README.md to create an event handler
 }
